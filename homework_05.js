e016bef995ba18144ace77ca61133daf51c074e6
Ingrid <tsam011@tecnicosuperiorekennedy.it>
2015-11-24 14:37:59 +0100

    ultima modif

--- homework_05.js
+++ homework_05.js
@@ -1,24 +1,23 @@
 
 function Stack() {
     this.myarray = []
-    Stack.prototype.push = function(e) { this.myarray.push(e); }
-    Stack.prototype.pop = function() { return this.myarray.pop(); }
-    Stack.prototype.peek = function() { return this.myarray[this.myarray.length - 1]; }
-    Stack.prototype.isEmpty = function() { return this.myarray.length == 0; }
 }
 
-function everyString(value) {
-    value = value.split(' ')
-    return value;   
-}
+Stack.prototype.push = function(e) { this.myarray.push(e); }
+Stack.prototype.pop = function() { return this.myarray.pop(); }
+Stack.prototype.peek = function() { return this.myarray[this.myarray.length - 1]; }
+Stack.prototype.isEmpty = function() { return this.myarray.length == 0; }
+
+
  
 
 
-function rpn(myA) {  
+function rpn(str) {    
+    var myA = str.split(' ');  
     var stack = new Stack();
     for(var i = 0; i < myA.length; i++){
         if(parseInt(myA[i]) > 0){
-            stack.push(parseInt(myA.pop()));        
+            stack.push(parseInt(myA[i]));        
         } else {
             var a = stack.pop();
             var b = stack.pop();
